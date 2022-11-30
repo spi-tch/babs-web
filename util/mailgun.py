7acbecde46078cbd786bb124883e2ef77abee730
temi <temi.ayo.babs@gmail.com>
2022-11-30 10:22:31 +0100

    Resources

--- util/mailgun.py
+++ util/mailgun.py
@@ -7,6 +7,9 @@ import requests
 MAILGUN_API_KEY = os.getenv("MAILGUN_API_KEY")
 logger = logging.getLogger(__name__)
 
+with open("static/welcome.html") as f:
+  html_text = f.read()
+
 
 def send_email(email: str):
   """Send email to user, use requests.post() to send email."""
@@ -14,10 +17,16 @@ def send_email(email: str):
     requests.post(
       "https://api.mailgun.net/v3/babs.ai/messages",
       auth=("api", MAILGUN_API_KEY),
-      data={"from": "Babs <admin@babs.ai>",
+      data={"from": "Babs <welcome@babs.ai>",
             "to": [email],
             "subject": "Hello from Babs",
-            "html": "Testing Babs mailing!"})
+            "html": html_text})
+
+    # Add user to mailing list
+    requests.post(
+        "https://api.mailgun.net/v3/lists/welcome@babs.ai/members",
+        auth=("api", MAILGUN_API_KEY),
+        data={"subscribed": True,
+              "address": [email]})
   except Exception as e:
     logger.error("Unable to send email.", e)
-
