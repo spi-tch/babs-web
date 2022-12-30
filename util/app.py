6c55dd80b05b21dfa4ff13d8eba69eff33fd69f4
Temi Babs <temi.ayo.babs@gmail.com>
2022-12-30 07:06:54 +0300

    Update app.py

--- util/app.py
+++ util/app.py
@@ -47,6 +47,7 @@ def register_blueprint(app):
   app.register_blueprint(routes.admin.find)
   app.register_blueprint(routes.auth.auth)
   app.register_blueprint(routes.channel.channel)
+  app.register_blueprint(routes.apps.apps)
 
   app.config['CORS_HEADERS'] = 'Content-Type'
   # Home
