c67d97bf7cab15d4ce8c8509a2d0099dea67b0de
temi <temi.ayo.babs@gmail.com>
2023-01-31 10:17:27 +0100

    Updated things

--- routes/apps.py
+++ routes/apps.py
@@ -20,7 +20,8 @@ auth_service = services.AuthService()
 user_service = services.UserService()
 
 SCOPES = ["https://www.googleapis.com/auth/calendar",
-          "https://www.googleapis.com/auth/gmail.modify",
+          "https://www.googleapis.com/auth/gmail.send",
+          "https://www.googleapis.com/auth/gmail.readonly"
           "https://www.googleapis.com/auth/contacts",
           "openid", "https://www.googleapis.com/auth/userinfo.email",
           "https://www.googleapis.com/auth/userinfo.profile"]
