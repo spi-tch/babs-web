72cbe79c8a2afb285a324169e30326bf48c469e9
temi <temi.ayo.babs@gmail.com>
2023-03-11 18:45:52 +0100

    Bugfix on auth callback

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/sh
 
 # Start the server
-SCRIPT_NAME=$BABS_VERSION gunicorn --bind 0.0.0.0:5000 --worker-class=sync --workers 9 --timeout 0 --preload app:app
+SCRIPT_NAME=/$BABS_VERSION gunicorn --bind 0.0.0.0:5000 --worker-class=sync --workers 9 --timeout 0 --preload app:app
